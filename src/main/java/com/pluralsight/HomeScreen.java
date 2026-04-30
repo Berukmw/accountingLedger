a18a8e7d14af349f85f38ef191d57193a3d66d59
Beruk <bmwubeshet@gmail.com>
2026-04-30 14:50:15 -0700

    home screen functionaility

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -1,4 +1,41 @@
 package com.pluralsight;
 
+import java.util.*;
+
 public class HomeScreen {
+
+    private Scanner scanner;
+    private ArrayList<Transaction> transactions;
+
+    public HomeScreen(Scanner scanner, ArrayList<Transaction> transactions) {
+        this.scanner = scanner;
+        this.transactions = transactions;
+    }
+
+    public void display() {
+
+        String input = "";
+
+        while (!input.equals("X")) {
+            System.out.println("\n----- HOME SCREEN -----");
+            System.out.println("D) Add Deposit");
+            System.out.println("P) Make Payment");
+            System.out.println("L) Ledger");
+            System.out.println("X) Exit");
+            System.out.print("\nEnter option: ");
+
+            input = scanner.nextLine().toUpperCase();   // makes characters uppercase
+
+            if (input.equals("D")) {
+                System.out.println("Add Deposit coming soon...");
+            } else if (input.equals("P")) {
+                System.out.println("Make Payment coming soon...");
+            } else if (input.equals("L")) {
+                System.out.println("Ledger coming soon...");
+            } else if (input.equals("X")) {
+                System.out.println("Goodbye!");
+
+            }
+        }
+    }
 }
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,16 +1,25 @@
 package com.pluralsight;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
 
         ArrayList<Transaction> transactions = FileManager.loadTransactions();
+        Scanner scanner = new Scanner(System.in);
+
+        HomeScreen homeScreen = new HomeScreen(scanner, transactions);
+        homeScreen.display();
+
+        scanner.close();
+
+       /*
 
         for (int i = 0; i < transactions.size(); i++) {
             Transaction t = transactions.get(i);
             System.out.println(t.getDate() + " | " + t.getVendor() + " | " + t.getAmount());
-        }
+
+        */
     }
 }
